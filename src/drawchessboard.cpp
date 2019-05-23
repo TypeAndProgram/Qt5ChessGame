6e788e9578344d7e58f701c3c5bc476883ce569c
TypeAndProgram <46855713+TypeAndProgram@users.noreply.github.com>
2019-05-23 17:19:37 -0500

    modify paths to piece images

--- src/drawchessboard.cpp
+++ src/drawchessboard.cpp
@@ -89,7 +89,7 @@ void DrawChessBoard::drawChessBoard(QGraphicsScene& scene, ChessPiece pieces[])
     for (int i = 0; i < 8; i++) {
 	// Set the name and pixmap for each white pawn
         pieces[i].setPieceName("Pawn");
-        pieces[i].setPixmap(QPixmap("../ImagesOfPieces/Pawn.png").scaled(80, 80));
+        pieces[i].setPixmap(QPixmap("ImagesOfPieces/Pawn.png").scaled(80, 80));
 
 	// Handle the first white pawn
         if (i == 0) {
@@ -109,42 +109,42 @@ void DrawChessBoard::drawChessBoard(QGraphicsScene& scene, ChessPiece pieces[])
 
     // Handle the other pieces \/ \/ \/
 
-    pieces[8].setPixmap(QPixmap("../ImagesOfPieces/Rook.png").scaled(80, 80));
+    pieces[8].setPixmap(QPixmap("ImagesOfPieces/Rook.png").scaled(80, 80));
     pieces[8].setPos(-200, -200);
     pieces[8].setPieceName("Rook");
     scene.addItem(&pieces[8]);
 
-    pieces[9].setPixmap(QPixmap("../ImagesOfPieces/Rook.png").scaled(80, 80));
+    pieces[9].setPixmap(QPixmap("ImagesOfPieces/Rook.png").scaled(80, 80));
     pieces[9].setPos(360, -200);
     pieces[9].setPieceName("Rook");
     scene.addItem(&pieces[9]);
 
-    pieces[10].setPixmap(QPixmap("../ImagesOfPieces/Knight.png").scaled(80, 80));
+    pieces[10].setPixmap(QPixmap("ImagesOfPieces/Knight.png").scaled(80, 80));
     pieces[10].setPos(-120, -200);
     pieces[10].setPieceName("Knight");
     scene.addItem(&pieces[10]);
 
-    pieces[11].setPixmap(QPixmap("../ImagesOfPieces/Knight.png").scaled(80, 80));
+    pieces[11].setPixmap(QPixmap("ImagesOfPieces/Knight.png").scaled(80, 80));
     pieces[11].setPos(280, -200);
     pieces[11].setPieceName("Knight");
     scene.addItem(&pieces[11]);
 
-    pieces[12].setPixmap(QPixmap("../ImagesOfPieces/Bishop.png").scaled(80, 80));
+    pieces[12].setPixmap(QPixmap("ImagesOfPieces/Bishop.png").scaled(80, 80));
     pieces[12].setPos(-40, -200);
     pieces[12].setPieceName("Bishop");
     scene.addItem(&pieces[12]);
 
-    pieces[13].setPixmap(QPixmap("../ImagesOfPieces/Bishop.png").scaled(80, 80));
+    pieces[13].setPixmap(QPixmap("ImagesOfPieces/Bishop.png").scaled(80, 80));
     pieces[13].setPos(200, -200);
     pieces[13].setPieceName("Bishop");
     scene.addItem(&pieces[13]);
 
-    pieces[14].setPixmap(QPixmap("../ImagesOfPieces/King.png").scaled(80, 80));
+    pieces[14].setPixmap(QPixmap("ImagesOfPieces/King.png").scaled(80, 80));
     pieces[14].setPos(40, -200);
     pieces[14].setPieceName("King");
     scene.addItem(&pieces[14]);
 
-    pieces[15].setPixmap(QPixmap("../ImagesOfPieces/Queen.png").scaled(80, 80));
+    pieces[15].setPixmap(QPixmap("ImagesOfPieces/Queen.png").scaled(80, 80));
     pieces[15].setPos(120, -200);
     pieces[15].setPieceName("Queen");
     scene.addItem(&pieces[15]);
@@ -152,7 +152,7 @@ void DrawChessBoard::drawChessBoard(QGraphicsScene& scene, ChessPiece pieces[])
 
     for (int i = 16; i < 24; i++) {
 	// Set pixmap and names for the black pawns
-        pieces[i].setPixmap(QPixmap("../ImagesOfPieces/B_Pawn.png").scaled(80, 80));
+        pieces[i].setPixmap(QPixmap("ImagesOfPieces/B_Pawn.png").scaled(80, 80));
         pieces[i].setPieceName("B_Pawn");
 
 	// Handle first black pawn
@@ -171,42 +171,42 @@ void DrawChessBoard::drawChessBoard(QGraphicsScene& scene, ChessPiece pieces[])
         scene.addItem(&pieces[i]);
     }
 
-    pieces[24].setPixmap(QPixmap("../ImagesOfPieces/B_Rook.png").scaled(80, 80));
+    pieces[24].setPixmap(QPixmap("ImagesOfPieces/B_Rook.png").scaled(80, 80));
     pieces[24].setPos(-200, 360);
     pieces[24].setPieceName("B_Rook");
     scene.addItem(&pieces[24]);
 
-    pieces[25].setPixmap(QPixmap("../ImagesOfPieces/B_Rook.png").scaled(80, 80));
+    pieces[25].setPixmap(QPixmap("ImagesOfPieces/B_Rook.png").scaled(80, 80));
     pieces[25].setPos(360, 360);
     pieces[25].setPieceName("B_Rook");
     scene.addItem(&pieces[25]);
 
-    pieces[26].setPixmap(QPixmap("../ImagesOfPieces/B_Knight.png").scaled(80, 80));
+    pieces[26].setPixmap(QPixmap("ImagesOfPieces/B_Knight.png").scaled(80, 80));
     pieces[26].setPos(-120, 360);
     pieces[26].setPieceName("B_Knight");
     scene.addItem(&pieces[26]);
 
-    pieces[27].setPixmap(QPixmap("../ImagesOfPieces/B_Knight.png").scaled(80, 80));
+    pieces[27].setPixmap(QPixmap("ImagesOfPieces/B_Knight.png").scaled(80, 80));
     pieces[27].setPos(280, 360);
     pieces[27].setPieceName("B_Knight");
     scene.addItem(&pieces[27]);
 
-    pieces[28].setPixmap(QPixmap("../ImagesOfPieces/B_Bishop.png").scaled(80, 80));
+    pieces[28].setPixmap(QPixmap("ImagesOfPieces/B_Bishop.png").scaled(80, 80));
     pieces[28].setPos(-40, 360);
     pieces[28].setPieceName("B_Bishop");
     scene.addItem(&pieces[28]);
 
-    pieces[29].setPixmap(QPixmap("../ImagesOfPieces/B_Bishop.png").scaled(80, 80));
+    pieces[29].setPixmap(QPixmap("ImagesOfPieces/B_Bishop.png").scaled(80, 80));
     pieces[29].setPos(200, 360);
     pieces[29].setPieceName("B_Bishop");
     scene.addItem(&pieces[29]);
 
-    pieces[30].setPixmap(QPixmap("../ImagesOfPieces/B_King.png").scaled(80, 80));
+    pieces[30].setPixmap(QPixmap("ImagesOfPieces/B_King.png").scaled(80, 80));
     pieces[30].setPos(40, 360);
     pieces[30].setPieceName("B_King");
     scene.addItem(&pieces[30]);
 
-    pieces[31].setPixmap(QPixmap("../ImagesOfPieces/B_Queen.png").scaled(80, 80));
+    pieces[31].setPixmap(QPixmap("ImagesOfPieces/B_Queen.png").scaled(80, 80));
     pieces[31].setPos(120, 360);
     pieces[31].setPieceName("B_Queen");
     scene.addItem(&pieces[31]);
